ffb82a7232776ee3cc20c5afa4a685de1491b42a
Maxim2Baranov <136797983+Maxim2Baranov@users.noreply.github.com>
2024-01-20 22:02:22 +0600

    Комменты

--- src/main/java/com/example/optimization2/HelloApplication.java
+++ src/main/java/com/example/optimization2/HelloApplication.java
@@ -18,21 +18,21 @@ public class HelloApplication extends Application {
         //ошибка обьявлять надо неявно, у другого человека не запуститься (ты обьявляешь явно)
         //Image backgroundImage = new Image("C:\\Users\\Администратор\\Desktop\\Optimization2\\src\\main\\java\\pictures\\background.jpg");
         //Вот как нужно. Это обьявление неявное
-        Image backgroundImage = new Image(getClass().getResourceAsStream("/com/example/optimization2/background.jpg"));
-        ImageView backgroundImageView = new ImageView(backgroundImage);
+        //Image backgroundImage = new Image(getClass().getResourceAsStream("/com/example/optimization2/background.jpg"));
+        //ImageView backgroundImageView = new ImageView(backgroundImage);
 
         // Загрузка FXML
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
         // Создание корневого контейнера с фотографией в качестве заднего фона
         StackPane root = new StackPane();
-        root.getChildren().addAll(backgroundImageView, fxmlLoader.load());
+        //root.getChildren().addAll(backgroundImageView, fxmlLoader.load());
 
         // Создание сцены с корневым контейнером
         Scene scene = new Scene(root); // Размеры вашего экрана
 
         stage.setScene(scene);
-        //stage.setFullScreen(true); // Установка полноэкранного режима
+        //stage.setFullScreen(true);// Установка полноэкранного режима
         stage.show();
     }
 
